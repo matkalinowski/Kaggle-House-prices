f57926d10f204fdc008c2b86605bf58b44d0abef
Mateusz Kalinowski <mateusz.a.kalinowski@gmail.com>
2017-09-16 12:36:51 +0200

    Setting id as index. Data file deleted.

--- utils/dataManagers/Data.py
+++
@@ -1,9 +0,0 @@
-from utils.dataManagers.dataHelpers import NullsHandler
-
-
-class DataObject(object):
-    def __init__(self, data):
-        self.data = data
-
-    def handle_missing_values(self, num_type_handling, categoricals_handling):
-        return NullsHandler(self.data, num_type_handling, categoricals_handling).handle_missing_values()
--- utils/dataManagers/steward.py
+++ utils/dataManagers/steward.py
@@ -1,15 +1,24 @@
-from utils.dataManagers.Data import DataObject
 from pathlib import Path
+
 import pandas as pd
+
+from utils.dataManagers.dataHelpers import NullsHandler
 
 
 def _read_data(data_type):
     return pd.read_csv(Path('data/' + data_type + '.csv').absolute())
 
 
+def set_id_as_index(df):
+    df.index = df.Id
+    del df['Id']
+    return df
+
+
 def formulate_data(data_type, num_type_handling, categoricals_handling):
-    do = DataObject(_read_data(data_type))
-    return do.handle_missing_values(num_type_handling, categoricals_handling)
+    data = _read_data(data_type)
+    data_without_nulls = NullsHandler(data, num_type_handling, categoricals_handling).handle_missing_values()
+    return set_id_as_index(data_without_nulls)
 
 
 class DataSteward(object):
